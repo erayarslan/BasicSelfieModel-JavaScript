660a2e611a1345499af7603e212f3e7a4e3808db
Eray <erayarslan@users.noreply.github.com>
2015-07-23 15:34:47 +0300

    Update BasicSelfieModel.js

--- BasicSelfieModel.js
+++ BasicSelfieModel.js
@@ -15,43 +15,43 @@ var Instagram = function (gender) {
 
 Instagram.prototype.setAreYouFamous = function (areYouFamous) {
   this.areYouFamous = areYouFamous;
-}; 
+};
 
 Instagram.prototype.profile = function () {
   return this.areYouFamous ? "sexy" : "barzo";
 };
 
 var Selfie = function (gender) {
-	Instagram.apply(this, arguments);
-	this.showBoobs = false;
+  Instagram.apply(this, arguments);
+  this.showBoobs = false;
 };
 
 Selfie.prototype = Object.create(Instagram.prototype);
 Selfie.prototype.constructor = Selfie;
 
 Selfie.prototype.takePhoto = function (showBoobs) {
-	this.showBoobs = showBoobs;
+  this.showBoobs = showBoobs;
 
-	if (showBoobs) {
-		this.areYouFamous = true;	
-	}
+  if (showBoobs) {
+    this.areYouFamous = true;
+  }
 
-	if (this.gender === "male" && this.areYouFamous === false) {
-		this.like = 7;
-	} else if (this.gender === "male" && this.areYouFamous === true) {
-		this.like = 300 - barzoCount;
-	} else if (this.gender == "female") {
-		if (!this.areYouFamous) {
-			this.like = 20; // bff mode #on
-		} else if (this.areYouFamous) {
-			this.like = 700;
-		}
+  if (this.gender === "male" && this.areYouFamous === false) {
+    this.like = 7;
+  } else if (this.gender === "male" && this.areYouFamous === true) {
+    this.like = 300 - barzoCount;
+  } else if (this.gender == "female") {
+    if (!this.areYouFamous) {
+      this.like = 20; // bff mode #on
+    } else if (this.areYouFamous) {
+      this.like = 700;
+    }
 
-		while (this.showBoobs && barzoCount < 150) {
-			this.like += barzoCount;
-			barzoCount++;
-		}
-	} else {
-		throw new Error("Go away fuckin faggot.");
-	}
+    while (this.showBoobs && barzoCount < 150) {
+      this.like += barzoCount;
+      barzoCount++;
+    }
+  } else {
+    throw new Error("Go away fuckin faggot.");
+  }
 };
